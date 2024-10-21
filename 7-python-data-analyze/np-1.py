05fb0baca3b852ce07f008ab8d744809b93c36e0
Gloridust <Gloridust@gmail.com>
2024-10-21 09:15:36 +0800

    Update np-1.py

--- 7-python-data-analyze/np-1.py
+++ 7-python-data-analyze/np-1.py
@@ -2,17 +2,37 @@ import numpy as np
 import time
 
 def take_sub_deter(local_deter, row_to_delete, column_to_delete):
+    """
+    从给定矩阵中删除指定的行和列，生成子矩阵
+    
+    参数:
+    local_deter: 原始矩阵
+    row_to_delete: 要删除的行索引
+    column_to_delete: 要删除的列索引
+    
+    返回:
+    sub_deter: 生成的子矩阵
+    """
     sub_deter = []
     for i in range(len(local_deter)):
-        if i != row_to_delete:
+        if i != row_to_delete:  # 跳过要删除的行
             new_row = []
             for j in range(len(local_deter[i])):
-                if j != column_to_delete:
+                if j != column_to_delete:  # 跳过要删除的列
                     new_row.append(local_deter[i][j])
             sub_deter.append(new_row)
     return sub_deter
 
 def calc_deter(the_deter):
+    """
+    递归计算矩阵的行列式
+    
+    参数:
+    the_deter: 要计算行列式的矩阵
+    
+    返回:
+    det_value: 计算得到的行列式值
+    """
     n = len(the_deter)
     
     # 基本情况：1x1 矩阵
@@ -26,8 +46,11 @@ def calc_deter(the_deter):
     # 递归情况：nxn 矩阵 (n > 2)
     det_value = 0
     for j in range(n):
+        # 生成子矩阵，删除第一行和第j列
         sub_det = take_sub_deter(the_deter, 0, j)
+        # 计算代数余子式
         cofactor = the_deter[0][j] * ((-1) ** j)
+        # 递归计算子矩阵的行列式，并累加到结果中
         det_value += cofactor * calc_deter(sub_det)
     
     return det_value
