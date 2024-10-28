7fd73fab398df6ae82b1b243b2ad0cce0d4e36fb
Gloridust <Gloridust@gmail.com>
2024-10-28 11:39:55 +0800

    Update class_1.py

--- 7-python-data-analyze/class_1.py
+++ 7-python-data-analyze/class_1.py
@@ -14,3 +14,7 @@ lisa = Student('Lisa Simpson', 87)
 
 bart.print_score()
 lisa.print_score()
+
+# 这个文件展示了一个简单的面向对象编程示例。我们定义了一个名为 Student 的类，
+# 该类有两个属性：name 和 score。类中有一个方法 print_score，用于打印学生的姓名和分数。
+# 然后，我们创建了两个 Student 对象，分别是 Bart Simpson 和 Lisa Simpson，并调用了 print_score 方法来打印他们的分数。
