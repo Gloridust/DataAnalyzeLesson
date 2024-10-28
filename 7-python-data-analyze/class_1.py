e4852d64204c56e3e1e540f02e723ccb853f87be
Gloridust <Gloridust@gmail.com>
2024-10-28 11:52:26 +0800

    Update class_1.py

--- 7-python-data-analyze/class_1.py
+++ 7-python-data-analyze/class_1.py
@@ -1,19 +1,24 @@
 # 面向对象练习
 
 class Student:
-    def __init__(self, name, score):
+    def __init__(self, name, score, gender):
         self.name = name
         self.score = score
+        self.gender = gender
     
     def print_score(self):
-        print('{0},{1}'.format(self.name, self.score))
+        print('{0},{1},{2}'.format(self.name, self.score, self.gender))
 
 
-bart = Student('Bart Simpson', 59)
-lisa = Student('Lisa Simpson', 87)
+bart = Student('Bart Simpson', 59, 'Male')
+lisa = Student('Lisa Simpson', 87, 'Female')
+alex = Student('Alex Johnson', 75, 'Gay')
+taylor = Student('Taylor Smith', 82, 'Transgender')
 
 bart.print_score()
 lisa.print_score()
+alex.print_score()
+taylor.print_score()
 
 # 这个文件展示了一个简单的面向对象编程示例。我们定义了一个名为 Student 的类，
 # 该类有两个属性：name 和 score。类中有一个方法 print_score，用于打印学生的姓名和分数。
