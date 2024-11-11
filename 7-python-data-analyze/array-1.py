d1384b28ed6d7ffad6d3df6db8136babef4f8de4
Gloridust <Gloridust@gmail.com>
2024-11-11 11:28:27 +0800

    Create array-1.py

---
+++ 7-python-data-analyze/array-1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a=np.array([1,2,3,4,5])
+b=np.array([1,2,3,4,5])
+c=np.array([[1,2,3,4,5],[1,2,3,4,5]])
+
+print(b)
+print(c)
+
+print(c.dtype)
+print(c.shape)
+print(c.ndim)
+print(c.size)
